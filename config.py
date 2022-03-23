70f9733572f380daf59e3cc58f752cb7d642545a
Zuzana Giertlova <zuzana.giertlova@m-c02fpg3pmd6m.lan>
2022-03-23 18:18:40 -0400

    moved hackathon repo here

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ app_config = {
     'ROOT_PATH': os.path.dirname(os.path.abspath(__file__))
 }
 
-mongo_uri = "mongodb+srv://crisis_app:pr37ErOV5LyBkZ7C@crisis-management-app.v7a3k.mongodb.net"
-realm_api_key = '62V3AOyla74WEYTUd8gjaLOx5v2BRg7DjrueGNtQLiJXjFDib7BJHtRaR5RPmjRC'
+mongo_uri = "mongodb+srv://admin:admin@cluster0.0mllm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+realm_api_key = 'jyUT7I7makns6pdoyEzLwGbNgkDRhUFJqwkfx7d6p9k4pqszrtnR2XAGoLxUgMPL'
 session_key = '1'
 secret_key = '1'
 jwt_secret = '1'
