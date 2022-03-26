c33c194b8d82faa26d5f185e6c3182c540a1ce98
zuzgiertl <38090804+zuzgiertl@users.noreply.github.com>
2022-03-26 11:24:14 -0400

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ app_config = {
     'ROOT_PATH': os.path.dirname(os.path.abspath(__file__))
 }
 
-mongo_uri = "mongodb+srv://admin:admin@cluster0.0mllm.mongodb.net/csa?retryWrites=true&w=majority"
-realm_api_key = 'oVitmXElDLqrpqoicnfu90LRt4JECvEgOcitkENSCj7n2d9eaZY0ZnBIouM9Ul2I'
+mongo_uri = ""
+realm_api_key = ''
 session_key = '1'
 secret_key = '1'
 jwt_secret = '1'
